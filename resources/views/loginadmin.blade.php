ac9676fa7b3da8b7d8a892653226ad175f0ab551
bagusadlan <bagusadlan444@gmail.com>
2021-04-30 23:29:03 +0700

    Menambah halaman login-admin

---
+++ resources/views/loginadmin.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/loginAdmin.css">
+    
+    <title>Login Admin</title>
+</head>
+<body>
+    
+        <div class="row justify-content-center">
+            <div class="justify-content-center">
+                    <img src="../img/thumb.jpg" class="mx-auto d-block rounded-circle icon" alt="...">
+                    <form>
+                        <div class="mb-3">
+                            <!-- <label for="exampleInputEmail1" class="form-label">Username</label> -->
+                            <input type="email" class="form-control text-center form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                            
+                        </div>
+                        <div class="mb-3">
+                            <!-- <label for="exampleInputPassword1" class="form-label">Password</label> -->
+                            <input type="password" class="form-control text-center form" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="row justify-content-center">
+                            <button type="submit" class="btn btn-primary submit">Submit</button>
+                        </div>
+                    </form>
+                
+            </div>
+        </div>
+    
+
+    <!-- ini font awesome -->
+    <link rel="stylesheet" href="css/all.css">
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.6.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/all.js"></script>
+</body>
+</html>
